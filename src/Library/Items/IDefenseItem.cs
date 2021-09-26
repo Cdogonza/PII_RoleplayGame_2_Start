6ee8c64277758bfe67996c1688aa1e5721be930f
Yonatan Morgades <yonatan@correo.ucu.edu.uy>
2021-09-26 19:23:01 -0300

    Tests and comments

--- src/Library/Items/IDefenseItem.cs
+++ src/Library/Items/IDefenseItem.cs
@@ -1,5 +1,7 @@
 namespace RoleplayGame
 {
+    /// Esta interfaz va a ser implementada por las clases items que posean la capacidad de tener un valor de defensa
+    
     public interface IDefenseItem
     {
         int DefenseValue{get;}
